2f4b9ec0c257d015154f6ac4f2e3eedee049e05e
Richard Chang <richardlwchang@gmail.com>
2018-02-19 16:26:48 -0800

    Move git tracking up several folders to get mu-plugins

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
-<?php get_header() ?>
-
-<h3>404 Error!</h3>
-<p>Sorry, the page you are looking for doesn't exist!</p>
-
-
-<?php get_footer() ?>
+<?php
+// Silence is golden.
---
+++ themes/welby-md-rchang/index.php
@@ -0,0 +1,7 @@
+<?php get_header() ?>
+
+<h3>404 Error!</h3>
+<p>Sorry, the page you are looking for doesn't exist!</p>
+
+
+<?php get_footer() ?>
