d9543ac2e968cf4eaff3844167f40bd8e7f6d977
Richard Chang <richardlwchang@gmail.com>
2018-04-06 17:39:50 -0700

    Modified the front-page and services page to use content from the wordpress dashboard [the_content()]

--- themes/welby-md-rchang/front-page.php
+++ themes/welby-md-rchang/front-page.php
@@ -1,40 +1,8 @@
 <?php get_header() ?>
-<div class="container">
-  <h2 class="headline--large headline--thin center--text">3 Steps to Getting a Second Medical Opinion</h2>
-  <div class="step__container row row--gutters--small row--equal-height-at-medium">
-    <div class="col--md-4">
-      <div class="step step--purple">
-        <div class="step__label">Step 1</div>
-        <div class="center--text">
-          <h2 class="headline no-margin--top">Choose Your Package</h2>
-          <i class="fa fa-stethoscope icon icon--medium"></i>
-          <p class="step__content">Choose from our various services.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col--md-4">
-      <div class="step step--yellow">
-        <div class="step__label">Step 2</div>
-        <div class="center--text">
-          <h2 class="headline no-margin--top">Fill Info</h2>
-          <i class="fa fa-id-card-o icon icon--medium"></i>
-          <p class="step__content">Fill out your personal, medical, and billing information.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col--md-4">
-      <div class="step step--green">
-        <div class="step__label">Step 3</div>
-        <div class="center--text">
-          <h2 class="headline no-margin--top">Upload Files</h2>
-          <i class="fa fa-upload icon icon--medium"></i>
-          <p class="step__content">Upload your X-ray's, MRI's, mammograms, and any other relevant medical records/reports.</p>
-          <p class="text--medium">And that's it!</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <a href="<?php echo site_url('/services') ?>" class="btn btn--blue btn--generic center--horizontal center--text">Get Started!</a>
-</div>
+<?php while (have_posts()) {
+  the_post(); ?>
 
+  <?php the_content() ?>
+
+<?php } ?>
 <?php get_footer() ?>
--- themes/welby-md-rchang/page-services.php
+++ themes/welby-md-rchang/page-services.php
@@ -1,80 +1,8 @@
 <?php get_header() ?>
 
-
-  <div class="row service">
-    <div class="col--md-4 center--text">
-      <h2 class="no-margin--top">Radiology Second Opinion</h2>
-      <i class="fa fa-stethoscope icon--medium"></i>
-      <p class="service__generic-text">Get second opinions from a radiology expert.</p>
-    </div>
-    <div class="col--md-8">
-      <a id="btn-rad-exp" href="#service-frame" data-link="https://welby.ambrahealth.com/share/stdreview">
-        <div class="btn btn--service btn--green">
-          <i class="fa fa-fighter-jet"></i> Expedited
-        </div>
-      </a>
-      <a id="btn-rad-std" href="#service-frame" data-link="https://welby.ambrahealth.com/share/24hrs">
-        <div class="btn btn--service btn--blue">
-          <i class="fa fa-envelope"></i> Standard
-        </div>
-      </a>
-      <!-- <a href="https://welby.ambrahealth.com/share/welbylanguage">
-        <div class="btn btn--service btn--blue"><i class="fa fa-globe"></i> Language Services</div>
-      </a> -->
-    </div>
-  </div>
-  <div class="row service">
-    <div class="col--md-4 center--text">
-      <h2 class="no-margin--top">Sports Medicine Second Opinion</h2>
-      <i class="fa fa-soccer-ball-o icon--medium"></i>
-      <p class="service__generic-text">Get advice from an experienced orthopedic surgeon.</p>
-    </div>
-    <div class="col--md-8">
-      <a id="btn-orth-exp" href="#service-frame" data-link="https://welby.ambrahealth.com/share/stdreview">
-        <div class="btn btn--service btn--green">
-          <i class="fa fa-fighter-jet"></i> Expedited
-        </div>
-      </a>
-      <a id="btn-orth-std" href="#service-frame" data-link="https://welby.ambrahealth.com/share/24hrs">
-        <div class="btn btn--service btn--blue">
-          <i class="fa fa-envelope"></i> Standard
-        </div>
-      </a>
-    </div>
-  </div>
-
-<!--
-  <div class="row service">
-    <div class="col--md-4 center--text">
-      <h2 class="no-margin--top">Video Consult</h2>
-      <i class="fa fa-video-camera icon--medium"></i>
-      <p>Want to speak to a medical professional in-person? Here you can have a live video consultation with an expert or a team of experts to address your concerns.</p>
-    </div>
-    <div class="col--md-8">
-      <div class="btn btn--service btn--green"><i class="fa fa-users"></i> One-on-one</div>
-      <div class="btn btn--service btn--blue"><i class="fa fa-user"></i> Team</div>
-    </div>
-  </div>
-
-  <div class="row service center--text">
-    <div class="col--md-4">
-      <h2 class="no-margin--top">Legal Case Review</h2>
-      <i class="fa fa-gavel icon--medium"></i>
-      <p>If you would like our help in a legal case or you need a medical expert testimony please contact us directly.</p>
-    </div>
-    <div class="col--md-8">
-      <a href="<?php echo site_url('contact') ?>">
-        <div class="btn btn--service btn--yellow">
-          Please contact us directly to let us know how we can help.
-        </div>
-      </a>
-    </div>
-  -->
-  </div>
-
-  <div>
-    <iframe id="service-frame" class="service__iframe"></iframe>
-  </div>
-
+<?php while (have_posts()) {
+  the_post(); ?>
+  <h1><?php the_title() ?></h1>
+<?php } ?>
 
 <?php get_footer() ?>
